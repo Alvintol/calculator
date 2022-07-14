b9e01f714ef34a42770def6a3c8a493a688ba28c
Alvin <alvin.tol@hotmail.com>
2022-07-14 16:44:14 -0600

    adds grid layout for buttons

--- src/App.js
+++ src/App.js
@@ -1,64 +1,63 @@
+import './css/main.css'
 
 const App = () => {
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
-      
-      <div id='main'>
-      Calculator Main
-      <div id='display'>
-      Screen
-      </div>
-      <div id='clear'>
-AC
-      </div>
-      <div id='divide'>
-/
-      </div>
-      <div id='times'>
-x
-      </div>
-      <div id='7'>
-7
-      </div>
-      <div id='8'>
-8
-      </div>
-      <div id='9'>
-9
-      </div>
-      <div id='minus'>
--
-      </div>
-      <div id='4'>
-4
-      </div>
-      <div id='5'>
-5
-      </div>
-      <div id='6'>
-6
-      </div>
-      <div id='plus'>
-+
-      </div>
-      <div id='1'>
-1
-      </div>
-      <div id='2'>
-2
-      </div>
-      <div id='3'>
-3
-      </div>
-      <div id='equal'>
-=
-      </div>
-      <div id='0'>
-0
-      </div>
-      <div id='dot'>
-.
-      </div>
+      <div className='w-1/2 h-1/2 border border-slate-300 grid grid-col-4 gap-1' id='main'>
+        <div className='border border-slate-300 col-start-1 col-end-4' id='display'>
+          Screen
+        </div>
+        <div className='border border-slate-300' id='clear'>
+          AC
+        </div>
+        <div className='border border-slate-300' id='divide'>
+          /
+        </div>
+        <div className='border border-slate-300' id='times'>
+          x
+        </div>
+        <div className='border border-slate-300' id='7'>
+          7
+        </div>
+        <div className='border border-slate-300' id='8'>
+          8
+        </div>
+        <div className='border border-slate-300' id='9'>
+          9
+        </div>
+        <div className='border border-slate-300' id='minus'>
+          -
+        </div>
+        <div className='border border-slate-300' id='4'>
+          4
+        </div>
+        <div className='border border-slate-300' id='5'>
+          5
+        </div>
+        <div className='border border-slate-300' id='6'>
+          6
+        </div>
+        <div className='border border-slate-300' id='plus'>
+          +
+        </div>
+        <div className='border border-slate-300' id='1'>
+          1
+        </div>
+        <div className='border border-slate-300' id='2'>
+          2
+        </div>
+        <div className='border border-slate-300' id='3'>
+          3
+        </div>
+        <div className='border border-slate-300' id='equal'>
+          =
+        </div>
+        <div className='border border-slate-300' id='0'>
+          0
+        </div>
+        <div className='border border-slate-300' id='dot'>
+          .
+        </div>
       </div>
     </div>
   );
