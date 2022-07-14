a2f6a4bf06d489398185c37a56dae0de7b314ec1
Alvin <alvin.tol@hotmail.com>
2022-07-14 16:09:36 -0600

    adds mock button div shells

--- src/App.js
+++ src/App.js
@@ -3,9 +3,63 @@ const App = () => {
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
       
-      Calculator
-      
-      
+      <div id='main'>
+      Calculator Main
+      <div id='display'>
+      Screen
+      </div>
+      <div id='clear'>
+AC
+      </div>
+      <div id='divide'>
+/
+      </div>
+      <div id='times'>
+x
+      </div>
+      <div id='7'>
+7
+      </div>
+      <div id='8'>
+8
+      </div>
+      <div id='9'>
+9
+      </div>
+      <div id='minus'>
+-
+      </div>
+      <div id='4'>
+4
+      </div>
+      <div id='5'>
+5
+      </div>
+      <div id='6'>
+6
+      </div>
+      <div id='plus'>
++
+      </div>
+      <div id='1'>
+1
+      </div>
+      <div id='2'>
+2
+      </div>
+      <div id='3'>
+3
+      </div>
+      <div id='equal'>
+=
+      </div>
+      <div id='0'>
+0
+      </div>
+      <div id='dot'>
+.
+      </div>
+      </div>
     </div>
   );
 }
