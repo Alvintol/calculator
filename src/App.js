5f6880ef4a63ca3dc48a15a2a7777d691fa325fb
Alvin <alvin.tol@hotmail.com>
2022-07-14 17:37:57 -0600

    styles buttons and adds equation to display

--- src/App.js
+++ src/App.js
@@ -3,59 +3,62 @@ import './css/main.css'
 const App = () => {
   return (
     <div className="app h-screen flex flex-col items-center justify-center">
-      <div className='w-1/2 h-1/2 border border-slate-300 grid grid-rows-6 grid-col-5 gap-1' id='main'>
-        <div className='flex flex-col justify-end text-end border border-slate-300 col-start-1 col-end-5' id='display'>
-          Screen
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 col-start-1 col-end-3' id='clear'>
-          AC
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 col-start-3' id='divide'>
-          /
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 col-start-4' id='times'>
-          x
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 col-start-1' id='7'>
-          7
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 col-start-2' id='8'>
-          8
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='9'>
-          9
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='minus'>
-          -
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='4'>
-          4
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='5'>
-          5
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='6'>
-          6
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='plus'>
-          +
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='1'>
-          1
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='2'>
-          2
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='3'>
-          3
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 row-span-2' id='equal'>
-          =
-        </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300 row-start-6 col-start-1 col-end-3' id='0'>
+      <div className='w-1/2 h-1/2 border border-slate-900 rounded-sm grid grid-rows-6 grid-col-5 gap-1 text-2xl p-1 bg-neutral-500' id='main'>
+        <div className='flex flex-col justify-end text-end col-start-1 col-end-5 px-2 text-4xl' id='display'>
+          <div className='text-orange-400 text-sm' id='equation'>
+            1x2
+          </div>
           0
         </div>
-        <div className='flex flex-col justify-center text-center border border-slate-300' id='dot'>
+        <div className='bg-red-400 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-1 col-end-3 font-bold' id='clear'>
+          AC
+        </div>
+        <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-3 font-bold' id='divide'>
+          /
+        </div>
+        <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-4 font-bold' id='times'>
+          x
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-1' id='7'>
+          7
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-2' id='8'>
+          8
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='9'>
+          9
+        </div>
+        <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='minus'>
+          -
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='4'>
+          4
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='5'>
+          5
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='6'>
+          6
+        </div>
+        <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='plus'>
+          +
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='1'>
+          1
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='2'>
+          2
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='3'>
+          3
+        </div>
+        <div className='bg-teal-500 flex flex-col justify-center text-center border border-slate-900 rounded-md row-span-2 font-bold' id='equal'>
+          =
+        </div>
+        <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md row-start-6 col-start-1 col-end-3' id='0'>
+          0
+        </div>
+        <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='dot'>
           .
         </div>
       </div>
