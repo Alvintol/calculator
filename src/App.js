2d03f9bafa800e8611370b491e8b2f3d6ce21134
Alvin <alvin.tol@hotmail.com>
2022-07-17 13:13:44 -0600

    replaces hardcoded button divs with rendered button components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import { StateProvider, useAppState } from './StateProvider'
 const App = () => {
 
   const state = useAppState();
+
   const buttons = state.buttons.map(btn => {
+    const { id, type, label } = btn;
 
     return (
       <Button
-      
+        key={id}
+        id={id}
+        type={type}
+        label={label}
       />
     )
   })
@@ -22,57 +27,6 @@ const App = () => {
         <div className='w-1/2 h-1/2 border border-slate-900 rounded-sm grid grid-rows-6 grid-col-5 gap-1 text-2xl p-1 bg-neutral-500' id='main'>
           <Display />
           {buttons}
-          <div className='bg-red-400 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-1 col-end-3 font-bold' id='clear'>
-            AC
-          </div>
-          <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-3 font-bold' id='divide'>
-            /
-          </div>
-          <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md col-start-4 font-bold' id='times'>
-            x
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='7'>
-            7
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='8'>
-            8
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='9'>
-            9
-          </div>
-          <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='minus'>
-            -
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='4'>
-            4
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='5'>
-            5
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='6'>
-            6
-          </div>
-          <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='plus'>
-            +
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='1'>
-            1
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='2'>
-            2
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md' id='3'>
-            3
-          </div>
-          <div className='bg-teal-500 flex flex-col justify-center text-center border border-slate-900 rounded-md row-span-2 font-bold' id='equal'>
-            =
-          </div>
-          <div className='bg-amber-500 flex flex-col justify-center text-center border border-slate-900 rounded-md row-start-6 col-start-1 col-end-3' id='0'>
-            0
-          </div>
-          <div className='bg-yellow-100 flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold' id='dot'>
-            .
-          </div>
         </div>
       </div>
     </StateProvider>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,12 @@ import classNames from 'classnames';
 
 const Button = ({id, label, type}) => {
   
-  const btnClass = classNames('flex flex-col justify-center text-center border border-slate-900 rounded-md font-bold', {
-    'bg-red-40 col-start-1 col-end-3': type === 'reset',
-    'bg-yellow-100': type === 'symbol',
-    'bg-amber-500 row-start-6 col-start-1 col-end-3': type === 'number' && id === 0,
+  const btnClass = classNames('flex flex-col justify-center text-center border border-slate-900 rounded-md', {
+    'bg-red-400 col-start-1 col-end-3 font-bold': type === 'reset',
+    'bg-yellow-100 font-bold': type === 'symbol',
+    'bg-amber-500 row-start-6 col-start-1 col-end-3': type === 'number' && id === '0',
     'bg-amber-500': type === 'number',
-    'bg-teal-500 row-span-2': type === 'equal'
+    'bg-teal-500 row-span-2 font-bold': type === 'equal'
   })
 
 return (
