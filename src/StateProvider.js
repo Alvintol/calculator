fec50714d324bf768a501233a1803fce17da2a37
Alvin <alvin.tol@hotmail.com>
2022-07-17 14:19:17 -0600

    adds clearDisplay function to stateprovider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,8 +3,10 @@ import { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext(appState)
 const CurrentNumberUpdateContext = createContext()
+const ClearDisplayContext = createContext()
 export const useAppState = () => useContext(StateContext)
 export const useCurrentNumberUpdate = () => useContext(CurrentNumberUpdateContext)
+export const useClearDisplayUpdate = () => useContext(ClearDisplayContext)
 
 export const StateProvider = ({ children }) => {
 
@@ -18,10 +20,15 @@ export const StateProvider = ({ children }) => {
         id : prev.currentNumber + id
     }))
 
+  const clearDisplay = () => setState(prev => ({ ...prev, currentNumber: 0 }))
+
   return (
     <StateContext.Provider value={state}>
       <CurrentNumberUpdateContext.Provider value={updateCurrentNumber}>
-        {children}
+        <ClearDisplayContext.Provider value={clearDisplay}>
+
+          {children}
+        </ClearDisplayContext.Provider>
       </CurrentNumberUpdateContext.Provider>
     </StateContext.Provider>
   )
