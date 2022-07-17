7986191de4eb4fb55844acd0bbe51312a9c4cfc9
Alvin <alvin.tol@hotmail.com>
2022-07-17 12:39:18 -0600

    imports stat einto display and input components

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,6 @@ import Input from './Input';
 const Display = () => {
 
   const state = useAppState();
-  console.log('STATE:', state)
 
   return (
     <div className='flex flex-col justify-end text-end col-start-1 col-end-5 px-2 text-4xl' id='display'>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,11 @@
+import { useAppState } from '../StateProvider'
+
 const Input = () => {
+  const state = useAppState()
+
   return (
     <div className='text-orange-400 text-sm' id='equation'>
-      1x5
+      {state.input}
     </div>
   )
 }
